406395c2d692cfffed1ef17b87a84d9092f87957
Popa Cosmin <poparobertcosmin@gmail.com>
2025-12-02 22:15:14 +0200

    w

---
+++ extension/config.js
@@ -0,0 +1,6 @@
+const PRODUCTION = true; // Set to true to use live server (autotext.zua.ro)
+
+const CONFIG = {
+    PRODUCTION: PRODUCTION,
+    API_URL: PRODUCTION ? "https://autotext.zua.ro/api" : "http://127.0.0.1:8000/api"
+};
